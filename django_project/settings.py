96953eb4ba55df3b29006fcc09bb3bee35ebeb14
kn4y4n <kn4y4nnn@gmail.com>
2023-02-24 14:33:04 +0530

    feat:CORS and CSRF Exempt

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,18 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # base
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",
+    "corsheaders",
+    # local
     "todos.apps.TodosConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +128,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+# forms
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
+
+
+admin.site.register(Todo, ToDoAdmin)
